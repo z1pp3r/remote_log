750ca280cb59a73fa62f8c3d78a5c161a76c456a
z1pp3r <z1pp3r90@gmail.com>
2012-07-26 23:47:03 +0400

    ururu

--- remote_log.py
+++ remote_log.py
@@ -1,36 +1,40 @@
 import os
 import sys
-from itertools import repeat
+import time
+import threading
 
-class Parser():
-    def __init__(self,filename=None,blocksize = 1):
+class Parser(threading.Thread):
+    def __init__(self,filename=None,blocksize = 1024):
         self.__filename = filename
         self.__file_inode = 0
         self.__fd = None
         self.__block_size = blocksize
 
     def __del__(self):
-        self.__closeFile()
+        self.__closeFile(silent=True)
 
     def __handleData(self):
         pass
 
     def __reload_file(self):
-        self.__closeFile()
+        if self.__fd is not None:
+            self.__closeFile()
         self.__loadFile()
 
-    def __loadFile(self):
+    def __loadFile(self,silent=False):
         try:
             self.__fd = os.open(self.__filename,os.O_RDONLY|os.O_APPEND)
+            self.__file_inode = os.fstat(self.__fd).st_ino
         except Exception,msg:
-            sys.stderr.write('Error opening file: '+str(msg)+'\n')
+            if not silent: sys.stderr.write('Error opening file: '+str(msg)+'\n')
             self.__fd = None
+            time.sleep(1)
 
-    def __closeFile(self):
+    def __closeFile(self,silent=False):
         try:
             os.close(self.__fd)
         except Exception,msg:
-            sys.stderr.write('Error closing file: '+str(msg)+'\n')
+            if not silent: sys.stderr.write('Error closing file: '+str(msg)+'\n')
 
     def getFileName(self):
         return self.__filename
@@ -44,8 +48,16 @@ class Parser():
     def getDataHandler(self):
         return self.__handleData
 
-    def test(self,string):
-        self.__handleData(string)
+    def test(self):
+        __msg = []
+        __result = True
+        if (self.__filename is None):
+            __msg.append('Filename is empty')
+            __result = False
+        if (self.__block_size<=0):
+            __msg.append('Block size must br greater than zero')
+            __result = False
+        return (__result,';'.join(__msg))
 
     def setBlockSize(self,size):
         self.__block_size = size
@@ -55,7 +67,7 @@ class Parser():
 
     def __isFileExists(self):
         try:
-            __cur_fd =  os.fstat(self.__filename).st_ino
+            __cur_fd =  os.stat(self.__filename).st_ino
         except:
             return False
         else:
@@ -64,21 +76,18 @@ class Parser():
             else:
                 return True
 
-    def __check(self):
-        if self.__filename is None:
-            raise AttributeError('Error: filename cannot be empty')
-            return False
-        if self.__block_size<=0:
-            raise AttributeError('Error: block size must be greater than 1')
-            return False
-        return True
 
     def start(self):
-        if not self.__check():
+        __status_ok,_ = self.test()
+        if not __status_ok:
             pass
         else:
-            self.__loadFile()
-            print 1
             old_data = ''
-            if not self.__isFileExists():
-                self.__reload_file()
+            while True:
+                if not self.__isFileExists():
+                    self.__reload_file()
+                data = os.read(self.__fd,self.__block_size)
+                if len(data)>0:
+                    print data
+                elif len(data)<self.__block_size:
+                    time.sleep(0.5)
